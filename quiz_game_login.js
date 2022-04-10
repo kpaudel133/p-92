53ef837caf19c6f7e3c1d5ea7d00951b6aad2450
kpaudel133 <75652036+kpaudel133@users.noreply.github.com>
2022-04-10 17:28:40 -0600

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,12 @@
+function addUser(){
+    
+    player_name_1 = document.getElementById("player1_name_input").value;
+
+    localStorage.setItem("player1_name_input", player_name_1);
+
+    player_name_2 = document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("player2_name_input", player_name_2);
+
+    window.location = "quiz_game_page.html";
+}
